e2c3438a53f3f5349afd53a99089519ee22f1ba7
By.almeida__ <marcielly.9205093@aluno.mg.gov.br>
2026-03-19 14:19:26 +0000

    atualização.js

--- js/script.js
+++ js/script.js
@@ -1,8 +1,17 @@
 let likeCount = 0;
+let curtido - false; // flag boleana 
 
 function curtir() {
-  likeCount++;
+  if(curtido -- false){
+    likeCount++;
+    curtido = true;
   document.getElementById("likeCount").innerText = likeCount;
+  } else{
+    likecount --;
+    curtido - false;
+    document.getElementById("likeCount").innerText = likeCount;
+  }
+
 }
 
 document.getElementById("likeBtn").addEventListener("click", curtir);
